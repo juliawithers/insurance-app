e522ab60c668ff98b0ae69299024fee981a47e9d
juliawithers <57202030+juliawithers@users.noreply.github.com>
2020-01-17 15:14:56 -0500

    Updated the CORS for BetterDoctor
    The issue stemmed from CORS and implementing a proxy should fix the errors

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 let apiKey_BD = '4b7a78e632642f4b6da68fcd56a2c6ae';
-let srcDoctors = 'https://api.betterdoctor.com/2016-03-01/doctors?sort=distance-asc&skip=0&limit=15&';
+let srcDoctors = 'https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?sort=distance-asc&skip=0&limit=15&';
 let srcPredictive = 'https://www.mapquestapi.com/search/v3/prediction?key=c77LD6NXniLCkBGt4rVOjzK7RsNokvAA&collection=address&languageCod=en&limit=5&q=';
 let srcMap ='https://www.mapquestapi.com/geocoding/v1/address?key=c77LD6NXniLCkBGt4rVOjzK7RsNokvAA&location=';
 
